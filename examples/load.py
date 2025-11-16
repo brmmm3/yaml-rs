b173212287812e3b31f2d841b364edadffeaf992
Marty B. <mrbm74@gmail.com>
2025-11-16 16:11:30 +0100

    Update example load.py

--- examples/load.py
+++ examples/load.py
@@ -6,8 +6,7 @@ import yaml_rs
 
 pprint(yaml_rs.load("config.yaml"))
 
-with open("config.yaml", "rb") as F:
-    pprint(yaml_rs.load(F.read()))
+pprint(yaml_rs.load(Path("config.yaml").read_bytes()))
 
 with Path("config.yaml").open("rb") as config_file:
     pprint(yaml_rs.load(config_file))
